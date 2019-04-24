03cde0480d38fd6adc54e30bdea9689f8b2c8898
Dan Bockrath <dbockrathdev@gmail.com>
2019-04-24 08:34:36 -0400

    Changed the GUI class. Not gonna mess with SWT cause it's not a default library in eclipse.

--- src/apcs/GUI.java
+++ src/apcs/GUI.java
@@ -6,8 +6,26 @@
  */
 package apcs;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI
-{
+import javax.swing.JFrame;
+
+public class GUI extends JFrame implements ActionListener {
+
+    private Tile[][] map;
+    
+    public GUI(Tile[][] map) {
+        
+        this.map = map;
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        
+        
+    }
 
 }
