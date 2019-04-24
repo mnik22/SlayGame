5f8ad4391b20718b416596e91a5475826fd2d935
Dan Bockrath <dbockrathdev@gmail.com>
2019-04-24 08:50:58 -0400

    Added basic code to the driver so the gui will display. Made a panel appear for the gui

--- src/apcs/Driver.java
+++ src/apcs/Driver.java
@@ -6,7 +6,14 @@
  */
 package apcs;
 
-
 public class Driver {
+    
+    public static void main(String[] args) {
+        
+        Tile[][] map = new Tile[5][5];
+        GUI gui = new GUI(map);
+        gui.displayGame();
+        
+    }
 
 }
--- src/apcs/GUI.java
+++ src/apcs/GUI.java
@@ -6,18 +6,58 @@
  */
 package apcs;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame implements ActionListener {
 
+    /** Height of the game frame. */
+    private static final int DEFAULT_HEIGHT = 302;
+    /** Width of the game frame. */
+    private static final int DEFAULT_WIDTH = 800;
+    
+    private JPanel panel;
+    private Point[] tiles;
+    
     private Tile[][] map;
     
     public GUI(Tile[][] map) {
         
         this.map = map;
+        initDisplay();
+        
+    }
+    
+    public void displayGame() {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            
+            public void run() {
+                
+                setVisible(true);
+                
+            }
+            
+        });
+        
+    }
+    
+    private void initDisplay() {
+        
+        panel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+            }
+        };
+        
+        panel.setLayout(null);
+        panel.setPreferredSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
         
     }
     
