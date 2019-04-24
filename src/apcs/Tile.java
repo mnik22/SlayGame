993cf98950ea070fe70cb5c5007c5446c5f77e01
Dick Pochinko <rbaker2002@icloud.com>
2019-04-24 08:31:09 -0400

    Started Tile class.

--- src/apcs/Tile.java
+++ src/apcs/Tile.java
@@ -1,5 +1,45 @@
 package apcs;
 
 public class Tile {
-
+    
+    private Tile topLeft;
+    private Tile left;
+    private Tile bottomLeft;
+    private Tile topRight;
+    private Tile right;
+    private Tile bottomRight;
+    
+    private int horizontalPos;
+    private int verticalPos;
+    
+    private Team team;
+    
+    public Tile(int x, int y)
+    {
+        horizontalPos = x;
+        verticalPos = y;
+    }
+    
+    
+    public boolean isAdjacent(Tile t)
+    {
+        return true;
+    }
+    
+    public Team getTeam()
+    {
+        return team;
+    }
+    
+    public void setTeam(Team t)
+    {
+        team = t;
+    }
+    
+    public void setAdjacent(Tile[][] board)
+    {
+        
+    }
+    
+    
 }
