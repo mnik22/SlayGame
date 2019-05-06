77320553efd90efdc83cdc77aa82c703c39eef6f
Dan Bockrath <dbockrathdev@gmail.com>
2019-05-06 08:53:17 -0400

    Started making the .fxml gui

--- src/apcs/Driver.java
+++ src/apcs/Driver.java
@@ -7,8 +7,9 @@
 package apcs;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Driver extends Application {
@@ -20,11 +21,16 @@ public class Driver extends Application {
         
         try {
             
-            BorderPane root = new BorderPane();
-            Scene scene = new Scene(root,400,400);
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-            primaryStage.setScene(scene);
+            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            primaryStage.setTitle("Slay Game");
+            primaryStage.setScene(new Scene(root));
             primaryStage.show();
+            
+//            BorderPane root = new BorderPane();
+//            Scene scene = new Scene(root,400,400);
+//            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//            primaryStage.setScene(scene);
+//            primaryStage.show();
             
         } catch(Exception e) {
             
