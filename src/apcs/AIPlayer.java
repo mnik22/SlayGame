191ada18ec95420e0b1e2bdb53c2f9cc98f51ae0
Kyle <kembling32@fairport.org>
2019-05-17 08:40:44 -0400

    Added extremely basic AI playRound method

--- src/apcs/AIPlayer.java
+++ src/apcs/AIPlayer.java
@@ -7,22 +7,33 @@
 package apcs;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 
 public class AIPlayer extends Player
 {
  
+    private ArrayList<Territory> territories;
     
     public AIPlayer(Color c)
     {
         super(c);
+        territories = super.getTerritories();
     }
 
     public void playRound()
     {
         while(hasNextPlay())
         {
-//           for(Territory t: te)
+           for(Territory territory: territories)
+           {
+               ArrayList<Tile> tiles = territory.getTiles();
+               for (Tile tile : tiles)
+               {
+                   if(tile.hasUnit())
+                       territory.moveUnit(tile.getUnit(), tiles.get((int) (Math.random() * tiles.size())));
+               }
+           }
         }
         
     }
