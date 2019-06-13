e359817b83afd8aabcf99d07949396ffa14914f5
Kyle <kembling32@fairport.org>
2019-06-13 01:02:45 -0400

    Rewrote AI code

--- src/apcs/AIPlayer.java
+++ src/apcs/AIPlayer.java
@@ -27,71 +27,102 @@ public class AIPlayer extends Player
     public void playRound()
     {
         super.playRound();
-        while(!endTurn())
+//        while(!endTurn())
+//        {
+        for(int i = 0; i < territories.size(); i++)
         {
-            System.out.println("stuck here 6");
-           for(int i = 0; i < territories.size(); i++)
+            ArrayList<Tile> tiles = territories.get(i).getTiles();
+            for(int j = 0; j < tiles.size(); j++)
+            {
+                if(territories.get(i).canPurchaseUnits() && !tiles.get(j).hasUnit() && tiles.size() > 2)
+                {
+                    if(territories.get(i).getMoney() > 20)
+                    {
+                        int index = 0;
+                        while(index < tiles.size() && !(tiles.get(j).setUnit(new Castle(tiles.get(j)))))
+                        {
+                            index++;
+                        }
+                        territories.get(i).buyCastle();
+                    }
+                    else
+                    {
+                        int index = 0;
+                        while(index < tiles.size() && !(tiles.get(j).setUnit(new Peasant(tiles.get(j)))))
+                        {
+                            index++;
+                        }
+                        territories.get(i).buyPeasant();
+                    }
+                }
+            }
+        } 
+        for(int i = 0; i < territories.size(); i++)
+        {
+           ArrayList<Tile> tiles = territories.get(i).getTiles();
+           for(int j = 0; j < tiles.size(); j++)
            {
-               ArrayList<Tile> tiles = territories.get(i).getTiles();
-               for(int j = 0; j < tiles.size(); j++)
+               ArrayList<Tile> adjacents = territories.get(j).adjacentEnemyTiles();
+               if(tiles.get(j).hasMoveableUnit() && tiles.get(j).getUnit().canMove && tiles.size() > 2)
                {
-                   ArrayList<Tile> adjacents = territories.get(j).adjacentEnemyTiles();
-                   if(tiles.get(j).hasMoveableUnit())
+                   int index = 0;
+                   while(index < adjacents.size() && !(territories.get(i).moveUnit(tiles.get(j).getUnit(), adjacents.get(index))))
                    {
-                       int tileCount = 0;
-                       int rand = (int) (Math.random() * adjacents.size());
-                       while(tileCount < adjacents.size() && adjacents.get(rand).getProtection() > 0)
-                       {
-                           System.out.println("stuck here 7");
-                           rand = (int) (Math.random() * adjacents.size());
-                           tileCount++;
-                       }
-                       if(adjacents.get(rand) != null && adjacents.get(rand).getProtection() == 0) {
-                           System.out.println("stuck here 8");
-                           territories.get(i).moveUnit(tiles.get(j).getUnit(), adjacents.get(rand));
-                       }
-                       System.out.println("got here");
+                       index++;
                    }
-                   System.out.println("got here 2");
-//                   try
+               }
+           }
+       }
+           
+               
+               
+               
+               
+               
+               
+//               System.out.println("got here 3");
+//               System.out.println(territories.get(i).canPurchaseUnits());
+//               int maxNumTries = 0;
+//               while(territories.get(i).canPurchaseUnits())
+//               {
+//                   System.out.println("stuck here 1");
+//                   System.out.println("territories.get(i).getMoney() > 20 " + (territories.get(i).getMoney() > 20));
+//                   if(territories.get(i).getMoney() > 20)
 //                   {
-//                        Thread.sleep(10);
-//                   } catch (InterruptedException e)
-//                   {
-//                        e.printStackTrace();
+//                       int randTile = (int) (Math.random() * tiles.size());
+//                       int count = 0;
+//                       while(tiles.get(randTile) != null && tiles.get(randTile).hasUnit() && count < tiles.size())
+//                           randTile = (int) (Math.random() * tiles.size());System.out.println("stuck here 2");
+//                       if(tiles.get(randTile) != null && !tiles.get(randTile).hasUnit())
+//                       {
+//                           tiles.get(randTile).setUnit(new Castle(tiles.get(randTile)));
+//                           territories.get(i).buyCastle();
+//                       }
 //                   }
-               }
-               System.out.println("got here 3");
-               while(territories.get(i).canPurchaseUnits())
-               {
-                   System.out.println("stuck here 1");
-                   if(territories.get(i).getMoney() > 20)
-                   {
-                       int randTile = (int) (Math.random() * tiles.size());
-                       int count = 0;
-                       while(tiles.get(randTile) != null && tiles.get(randTile).getProtection() > 0 && count < tiles.size())
-                           randTile = (int) (Math.random() * tiles.size());System.out.println("stuck here 2");
-                       if(tiles.get(randTile) != null && tiles.get(randTile).getProtection() == 0)
-                       {
-                           tiles.get(randTile).setUnit(new Castle(tiles.get(randTile)));
-                           territories.get(i).buyCastle();
-                       }
-                   }
-                   else
-                   {
-                       int randTile = (int) (Math.random() * tiles.size());
-                       int count = 0;
-                       while(tiles.get(randTile) != null && tiles.get(randTile).getProtection() > 0 && count < tiles.size())
-                           randTile = (int) (Math.random() * tiles.size()); System.out.println("stuck here 3");
-                       if(tiles.get(randTile) != null && tiles.get(randTile).getProtection() == 0)
-                       {
-                    	   tiles.get(randTile).setUnit(new Peasant(tiles.get(randTile)));
-                    	   System.out.println("stuck here 4");
-                    	   territories.get(i).buyPeasant();
-                    	   System.out.println("stuck here 5");
-                       }
-                   }
-               }
+//                   else
+//                   {
+//                       int randTile = (int) (Math.random() * tiles.size());
+//                       int count = 0;
+//                       System.out.println(tiles.get(randTile));
+//                       System.out.println(tiles.get(randTile).hasUnit());
+//                       System.out.println(count < tiles.size());
+//                       
+//                       while(tiles.get(randTile) != null && tiles.get(randTile).hasUnit() && count < tiles.size()) {
+//                           randTile = (int) (Math.random() * tiles.size()); 
+//                           count++; 
+//                           System.out.println("stuck here 3"); 
+//                       }
+//                       System.out.println("testing");
+//                       System.out.println((tiles.get(randTile) != null) + " " + (!tiles.get(randTile).hasUnit()));
+//                       if(tiles.get(randTile) != null && !tiles.get(randTile).hasUnit())
+//                       {
+//                           System.out.println("stuck here 4");
+//                    	   tiles.get(randTile).setUnit(new Peasant(tiles.get(randTile)));
+//                    	   System.out.println("stuck here 5");
+//                    	   territories.get(i).buyPeasant();
+//                       }
+//                   }
+//               }
 //               try
 //               {
 //                    Thread.sleep(10);
@@ -99,7 +130,6 @@ public class AIPlayer extends Player
 //               {
 //                    e.printStackTrace();
 //               }
-           }
-        }
+//        }
     }
 }
